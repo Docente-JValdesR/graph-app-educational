0a26fcfc3af5d266c4caa2a9f17ac518b6d9a056
jose valdes <docente.josevaldes@gmail.com>
2023-03-15 22:12:44 -0300

    se agregan los modelos para compaginar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { VerticalBar } from "./Models/VerticalBar";
+import { HorizontalBar } from "./Models/HorizontalBar";
+import { StackerBar } from "./Models/StackerBar";
+import { Area } from "./Models/Area";
+import { LineChart } from "./Models/LineChart";
+import { MutiAxisLine } from "./Models/MultiAxisLine";
+import { PieChart } from "./Models/PieChart";
+import { DoughnutChart } from "./Models/DoughnutChart";
+import { PolarAreaChart } from "./Models/PolarAreaChart";
+import { RadarChart } from "./Models/RadarChart";
+import { ScatterChart } from "./Models/ScatterChart";
+import { BubbleChart } from "./Models/BubbleChart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row justify-content-center align-items-center g-2">
+        <div className="col-8">
+          <VerticalBar />
+        </div>
+        <div className="col-8">
+          <HorizontalBar />
+        </div>
+        <div className="col-8">
+          <StackerBar />
+        </div>
+        <div className="col-8">
+          <Area />
+        </div>
+        <div className="col-8">
+          <LineChart />
+        </div>
+        <div className="col-8">
+          <MutiAxisLine />
+        </div>
+        <div className="col-6">
+          <PieChart />
+        </div>
+        <div className="col-6">
+          <DoughnutChart />
+        </div>
+        <div className="col-6">
+          <PolarAreaChart />
+        </div>
+        <div className="col-6">
+          <RadarChart />
+        </div>
+        <div className="col-8">
+          <ScatterChart />
+        </div>
+        <div className="col-8">
+          <BubbleChart />
+        </div>
+        <div className="col-8"></div>
+      </div>
     </div>
   );
 }
